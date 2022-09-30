cd83cbce620cdf5391f8949f29763fdc64ed44ce
One2INF <goodbob256@gmail.com>
2022-09-30 17:02:53 +0800

    serialport：添加串口类

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDebug>
+#include <QtSerialPort/QSerialPortInfo>
+
+#include "serialport.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+  QApplication a(argc, argv);
+  MainWindow w;
+  w.show();
+
+  SERIALPORT_INFO_ST serialport_info;
+
+  if(argc == 3)
+  {
+    serialport_info.portname = (QString)argv[1];
+    serialport_info.baudrate = ((QString)argv[2]).toInt();
+  }
+  else
+  {
+    qDebug() << QString("Usage: %1 portnum baudrate!\r\n").arg(argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  serialport serialport;
+  serialport.init(&serialport_info);
+  serialport.start();
+
+  return a.exec();
 }
